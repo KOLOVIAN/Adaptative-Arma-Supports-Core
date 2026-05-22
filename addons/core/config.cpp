dfff4f9e328d1106a35cbc0c64cf4a043ad1d7b4
Nomas <78245023+Nomas-X@users.noreply.github.com>
2026-05-22 20:37:48 +0300

    Added `core` config file and needed files for macros

--- addons/core/config.cpp
+++ addons/core/config.cpp
@@ -1,18 +1,45 @@
+#include "script_component.hpp"
+
 class CfgPatches {
-    class aas_main { 
-        name = "AAS - Adaptative Arma Supports";
-        author = "KOLOVIAN";
-        requiredVersion = 1.60;
+	class ADDON {
+
+        // Meta information for editor
+		name = ADDON_NAME;
+
+        author = "$STR_aas_author";
+        authors[] = {"KOLOVIAN"};
+        
+        url = "$STR_aas_URL";
+
+		VERSION_CONFIG;
+
+        // Addon Specific Information
+        // Minimum compatible version. When the game's version is lower, pop-up warning will appear when launching the game.
+        requiredVersion = 2.02;
+
+        // Required addons, used for setting load order.
+        // When any of the addons is missing, pop-up warning will appear when launching the game.
         requiredAddons[] = {
+			QPVAR(main),
+			"cba_main",
             "A3_Data_F", 
             "A3_Modules_F", 
             "A3_Modules_F_Curator", 
             "cba_main", 
             "cba_settings",
             "3DEN" // Forces mod to load AFTER the Eden Editor
-        }; 
+			};
+
+		// Optional. If this is 1, if any of requiredAddons[] entry is missing in your game the entire config will be ignored and return no error (but in rpt) so useful to make a compat Mod (Since Arma 3 2.14)
+		skipWhenMissingDependencies = 1;
+        
+        // List of objects (CfgVehicles classes) contained in the addon. Important also for Zeus content (units and groups)
+        units[] = {};
+
+        // List of weapons (CfgWeapons classes) contained in the addon.
         weapons[] = {};
-    };
+
+	};
 };
 
 // =======================================================
---
+++ addons/core/script_component.hpp
@@ -0,0 +1,8 @@
+#define COMPONENT core
+
+// This is being used for the Addon's Name and can be "My Addon Template Framework"
+#define COMPONENT_BEAUTIFIED AAS Core
+
+
+#include "\z\aas\addons\main\script_mod.hpp"
+#include "\z\aas\addons\main\script_macros.hpp"
